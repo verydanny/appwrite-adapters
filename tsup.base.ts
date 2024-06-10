753ca5d050e4b5442f51c3a58bb49cade48260d5
Daniil Veremchuk <daniel@verydanny.com>
2024-06-10 13:13:50 -0400

    test4

--- tsup.base.ts
+++ tsup.base.ts
@@ -1,9 +1,22 @@
-import { defineConfig, type Options } from 'tsup'
+import { defineConfig, type Options } from "tsup"
+import { utimes } from "node:fs"
 
-export const shared = (config?: Options | Options[]) => defineConfig({
-    entry: ["src/**/*.{ts,tsx}"],
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    ...config
-})
+export const shared = (config?: Options | Options[]) =>
+    defineConfig({
+        entry: ["src/**/*.{ts,tsx}"],
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+        async onSuccess() {
+            if (process.env.npm_lifecycle_event === "dev") {
+                const time = new Date()
+                utimes(
+                    "../../path-to-the/app-needs-to-rebuild/src/index.tsx",
+                    time,
+                    time,
+                    () => {},
+                )
+            }
+        },
+        ...config,
+    })
