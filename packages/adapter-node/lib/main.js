c4e576a6d69ba5ac67cbf101f54a500815d54fa2
Daniil Veremchuk <daniel@verydanny.com>
2024-06-10 13:30:30 -0400

    test4

--- packages/adapter-node/lib/main.js
+++
@@ -1,7 +0,0 @@
-// src/main.ts
-var main_default = () => {
-  console.log("Work has commenced");
-};
-export {
-  main_default as default
-};
--- packages/adapter-node/src/main.ts
+++
@@ -1,3 +0,0 @@
-export default () => {
-    console.log("Work has commenced")
-}
