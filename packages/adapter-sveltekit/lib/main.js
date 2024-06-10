c9146206abb9aa31d70a98c0a9d74873698cb3c5
Daniil Veremchuk <daniel@verydanny.com>
2024-06-10 13:38:13 -0400

    changes

--- packages/adapter-sveltekit/lib/main.js
+++
@@ -1,9 +0,0 @@
-// src/main.ts
-import someTest from "@gravlabs/appwrite-adapter-node";
-someTest();
-var main_default = () => {
-  console.log("Work has commenced on bun");
-};
-export {
-  main_default as default
-};
--- packages/adapter-sveltekit/src/main.ts
+++ packages/adapter-sveltekit/src/main.ts
@@ -1,7 +1,3 @@
-import someTest from "@gravlabs/appwrite-adapter-node"
-
-someTest()
-
 export default () => {
     console.log("Work has commenced on bun")
 }
