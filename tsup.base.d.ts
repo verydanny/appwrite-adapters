ef3a6f97fe886248b271843984fa50950aeef685
Daniil Veremchuk <daniel@verydanny.com>
2024-06-10 12:45:23 -0400

    test3

---
+++ tsup.base.d.ts
@@ -0,0 +1,3 @@
+import { type Options } from 'tsup';
+export declare const shared: (config?: Options | Options[]) => Options | Options[] | ((overrideOptions: Options) => Options | Options[] | Promise<Options | Options[]>);
+//# sourceMappingURL=tsup.base.d.ts.map
