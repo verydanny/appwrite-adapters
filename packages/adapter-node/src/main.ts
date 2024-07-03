083d62324c62224133eb293f1a2dc1ec34511902
Daniil Veremchuk <daniel@verydanny.com>
2024-07-03 17:39:01 -0400

    better

--- packages/adapter-node/src/main.ts
+++ packages/adapter-node/src/main.ts
@@ -1,6 +1,5 @@
-import { Readable } from "node:stream"
 import { getRequestListener } from "./getRequestListener.js"
-import type { Context, CustomErrorHandler } from "./types.ts"
+import type { CustomErrorHandler } from "./types.ts"
 
 export function serve(
     app: import("hono").Hono,
@@ -13,34 +12,4 @@ export function serve(
     },
 ) {
     return getRequestListener(app.fetch, options)
-
-    // return async (context: Context) => {
-    //     const listener = initializeListener(context.error)
-
-    //     try {
-    //         const response = await listener(context.req, context.res)
-
-    //         if (response) {
-    //             const blob = await response.blob()
-
-    //             const headers = Object.fromEntries(response.headers.entries())
-
-    //             // This is only needed on Appwrite, if this isn't included
-    //             // then text and json-based routes will loop forever
-    //             if (!headers["content-length"] && blob.size) {
-    //                 headers["content-length"] = blob.size.toString()
-    //             }
-
-    //             headers["Cache-Control"] = "public,max-age=31536000"
-
-    //             return context.res.send(
-    //                 Readable.from(blob.stream()),
-    //                 200,
-    //                 headers,
-    //             )
-    //         }
-    //     } catch (e) {
-    //         context.error(e)
-    //     }
-    // }
 }
